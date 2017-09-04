8c826186e235c6a73d7acf826f9b9e2cf6d8c751
Nicolas Yang <=guangwei85@gmail.com>
2017-09-04 11:46:25 -0400

    Added functions

--- Ex1.js
+++ Ex1.js
@@ -1,6 +1,52 @@
+// (*) Determine if an number is an odd number
+function isOdd(num){
 
-// **
-//Find the largest number of the input array
+}
+
+// (*) Return true if a number is even
+function isEven(num){
+
+}
+
+
+// (*) Find the largest number of the input array
 function max(arr){
 
 }
+
+// (*) Find the sum of the input array
+// Ex [10,30,50] => 90
+function sum(arr){
+
+}
+
+// (**) Sum the odd numbers in the input array
+// Ex [3, 6, 8, 7] =>10
+function sumOdds(arr){
+
+}
+
+
+// (***)  Reverse a string
+// Ex "abcd" => "dcab"
+function reverse(str){
+
+}
+
+// (***) Sort an array with ascending order
+// Ex [8, 5, 10, 1] => [1,5,8,10]
+function sort(arr){
+
+}
+
+
+function repeat(str, num){
+
+}
+
+
+
+
+(function test(){
+
+})();
