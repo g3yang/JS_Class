8386ff301bade6e01ca494df7a891c676f1c1867
Nicolas Yang <guangwei85@gmail.com>
2018-06-03 23:09:14 -0400

    Lecture 4 Homework

---
+++ lecture_4_homework/module.js
@@ -0,0 +1,41 @@
+
+var module = {
+    /**
+     * difference([1, 2, 3], [100, 2, 1, 10]))
+     * Expected output [3,10, 100]
+     */
+    findDifference: function(arr1, arr2){
+
+    },
+    /**
+    * Get the union of two arrays
+    * Example: ([1,2,3],[2,3,4]) => [1,2,3,4]
+    */
+    getUnion: function(arr1, arr2){
+
+    },
+    /**
+     * Sum all odd numnbers
+     * Ex: [2,4,5,7] =>12
+     */
+    sumOdds: function(arr){
+
+    },
+    /**
+     * Check if a string is palidrome.
+     *  Ex: ada=> true, illi=>true, kill => false
+     */
+    isPalindrome: function(s){
+
+    },
+    /**
+     * Return a new array that concate the first name and last name, 
+     * then make it upper case
+     * Ex [{firstName:'Joe', lastName:'Smith}] => [{name: 'JOE SMITH'}]
+     */
+     getFullNames: function(persons){
+
+     }
+};
+
+module.exports = module;
---
+++ lecture_4_homework/module.test.js
@@ -0,0 +1,20 @@
+var module = require('./module');
+
+describe('Test palindrom',()=>{
+    it('A simgle character',()=>{
+        expect(module.isPalindrome('a')).toEqual(true);
+    })
+    /**
+     * Add more testcases
+     */
+});
+
+describe('Test Get Full Names',()=>{
+    /**
+     * Add your test cases
+     */
+});
+
+/**
+ * Add all other test cases
+ */
